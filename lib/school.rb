c893138852589bd0bb01192110967e7e6396a5db
Elise Phan <aliceconnic@gmail.com>
2018-12-04 05:50:00 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,18 @@
 # code here!
+class School
+  attr_reader :school_name, :student_name, :grade 
+
+  def roster
+    roster = {}
+  end
+  
+  def initialize(school_name)
+    @school_name = school_name
+  end
+  
+ def add_student (student_name, grade)
+  @student_name = student_name
+  @grade = grade
+ end
+ 
+end
